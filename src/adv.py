e13b5e2fe552c62738f68ec8d5ec960d2f558079
justinelai <justine.lai@gmail.com>
2019-12-04 13:37:11 -0500

    make room and player classes

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -43,9 +44,15 @@ room['treasure'].s_to = room['narrow']
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
+
+def current():
+
+
 # * Waits for user input and decides what to do.
-#
+
 # If the user enters a cardinal direction, attempt to move to the room there.
+
+
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
